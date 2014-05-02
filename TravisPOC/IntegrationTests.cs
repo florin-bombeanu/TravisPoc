1c28b0cc44dda396d86d6f310452763730733f2f
Florin Bombeanu <florin_pch@yahoo.com>
2014-05-02 15:49:47 +0300

    Added integration test

---
+++ TravisPOC/IntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace TravisPOC
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+
+        public void TestThatCalinIsInDb()
+        {
+            //arrange
+            var repo = new Repository();
+
+            //act
+            var result = repo.FetchUserByName("Calin");
+
+            //assert
+            Assert.That(result, Is.True);
+        }
+
+        public void TestThatMihaiIsNotInDb()
+        {
+            //arrange
+            var repo = new Repository();
+
+            //act
+            var result = repo.FetchUserByName("Mihai");
+
+            //assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
---
+++ TravisPOC/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace TravisPOC
+{
+    public class Repository
+    {
+        public bool FetchUserByName(string name)
+        {
+            const string connectionString = "Server=localhost;Database=travisPoc;Uid=root;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var command = new MySqlCommand(String.Format("Select * from Users where Name = {0}", name));
+                connection.Open();
+
+                var ds = new DataSet("set");
+                var da = new MySqlDataAdapter(command);
+
+                da.Fill(ds);
+                if (ds.Tables[0] != null && ds.Tables[0].Rows.Count == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
