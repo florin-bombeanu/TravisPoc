c318678d7ec2756d9396a774077f62c701028994
Florin Bombeanu <florin_pch@yahoo.com>
2014-05-02 15:07:52 +0300

    initial commit

---
+++ TravisPOC/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace TravisPOC
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            //arrange 
+            const int a = 1;
+            const int b = 2;
+            var calculator = new Calculator();
+
+            //act
+            var result = calculator.Add(a, b);
+
+            //assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
